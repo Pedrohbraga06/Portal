0e31aa98a6d8c32e8096d4b422b425e3c73cda46
Pedro <pe.h.braga06@gmail.com>
2021-03-23 20:00:34 -0300

    teste

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (isset($_SESSION['id']) && empty($_SESSION['id']) == false) {
-    echo "Area restrita... ";
+    echo "Área restrita... ";
     
 } else {
   header("Location: login.php");
