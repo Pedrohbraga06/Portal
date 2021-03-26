75c4a53d5f62fd04a935b84b452ec9005563b9b4
Pedro <pe.h.braga06@gmail.com>
2021-03-26 18:36:12 -0300

    pronto

--- projeto_ordenar/index.php
+++ projeto_ordenar/index.php
@@ -5,14 +5,30 @@ try {
   echo "ERRO: ".$e->getMessage();
   exit;
 }
+if (isset($_GET['ordem']) && !empty($_GET['ordem'])) {
+  $ordem = addslashes($_GET['ordem']);
+  $sql = "SELECT * FROM usuarios ORDER BY ".$ordem;
+} else {
+  $ordem = "";
+  $sql = "SELECT * FROM usuarios";  
+}
 ?>
+<form method="GET">
+  <select name="ordem" onchange="this.form.submit()">
+    <option></option>
+    <option value="nome"<?php echo ($ordem=="nome")?'selected="selected"':''; ?> >Pelo nome</option>
+    <option value="idade"<?php echo ($ordem=="idade")?'selected="selected"':''; ?> >Pela idade</option>
+  </select>
+</form>
+
 <table border="1" width="400">
   <tr>
     <th>Nome</th>
     <th>Idade</th>
   </tr>
   <?php
-  $sql = "SELECT * FROM usuarios";
+  
+
   $sql = $pdo->query($sql);
   if($sql->rowCount() > 0){
 
