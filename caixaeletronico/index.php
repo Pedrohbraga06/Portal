856f730dc7eca300a6a6c9e61cdee2f870310e0d
Pedro <pe.h.braga06@gmail.com>
2021-04-07 17:21:06 -0300

    isso ai ta comitado viu

---
+++ caixaeletronico/index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+
+<html>
+ <head>
+  <title>Caixa Eletronico</title>
+ </head>
+ <body>
+    <h1>Banco</h1>
+    Agencia: 0000<br/>
+    Conta: 0000<br/>
+    <a href="sair.php"></a>
+</body>
+</html>
